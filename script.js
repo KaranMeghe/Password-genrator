c6a5f3af5cf6cbf28e0c2b3bb0452b06c566d15a
Karan Meghe <karanmeghe87@gmail.com>
2022-11-13 19:27:14 +0530

    refactor code

--- script.js
+++ script.js
@@ -1,69 +1,80 @@
-// Getting DOM elements
-const form = document.getElementById('passwordGenratorForm');
+// Getting the DOM Eleements
 const resultDOM = document.getElementById('result');
-const copyBtnDOM = document.getElementById('copy');
+const copybtnDOM = document.getElementById('copy');
 const lengthDOM = document.getElementById('length');
-const upperCaseDOM = document.getElementById('uppercase');
+const uppercaseDOM = document.getElementById('uppercase');
 const numbersDOM = document.getElementById('numbers');
 const symbolsDOM = document.getElementById('symbols');
-const genratebtn = document.getElementById('genrate');
+const generatebtn = document.getElementById('generate');
+const form = document.getElementById('passwordGenratorForm');
 
-//Chracter Code Genrating Function
-function arrayFromLowToHigh(low,high){
-    const array = [];
-    for(let i = low; i <= high.length; i++){
-        array.push(i);
-    }
-    return array;
- } 
+// Generating Character Codes For The Application 
+const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
+const NUMBER_CODES = arrayFromLowToHigh(48, 57);
+const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
 
-// Getting character codes For The Application
-const UPPERCASE_CODES = arrayFromLowToHigh(65,90);
-const LOWERCASE_CODES = arrayFromLowToHigh(97,122);
-const NUMBERS_CODES = arrayFromLowToHigh(48,57);
-const SYMBOLS_CODES = arrayFromLowToHigh(33,47)
-                     .concat(arrayFromLowToHigh(58,64))
-                     .concat(arrayFromLowToHigh(91,96))
-                     .concat(arrayFromLowToHigh(123,126));
+// Copy Password
+copybtnDOM.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const passwordToCopy = resultDOM.innerText;
 
+  // Edge Case when Password is Empty
+  if (!passwordToCopy) return;
 
+  // Copy Functionality
+  textarea.value = passwordToCopy;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  textarea.remove();
+  alert('Password Copied to Clipboard');
+});
 
-// The Password Genrating function
-let genratePassword = (
+// Checking the options that are selected and setting the password
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const characterAmount = lengthDOM.value;
+  const includeUppercase = uppercaseDOM.checked;
+  const includeNumbers = numbersDOM.checked;
+  const includeSymbols = symbolsDOM.checked;
+  const password = generatePassword(
     characterAmount,
-    includeUpperCase,
+    includeUppercase,
     includeNumbers,
     includeSymbols
+  );
+  resultDOM.innerText = password;
+});
+
+// The Password Generating Function
+let generatePassword = (
+  characterAmount,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
 ) => {
-    let charCodes = LOWERCASE_CODES;
-    if(includeUpperCase) charCodes = charCodes.concat(UPPERCASE_CODES);
-    if(includeNumbers) charCodes = charCodes.concat(NUMBERS_CODES);
-    if(includeSymbols) charCodes = charCodes.concat(SYMBOLS_CODES);
+  let charCodes = LOWERCASE_CODES;
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CODES);
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CODES);
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CODES);
+  const passwordCharacters = [];
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode =
+      charCodes[Math.floor(Math.random() * charCodes.length)];
+    passwordCharacters.push(String.fromCharCode(characterCode));
+  }
+  return passwordCharacters.join('');
+};
 
-    const passWordCharacters = [];
-    for(let i = 0; i < characterAmount; i++){
-        const charCode = charCodes[Math.floor(Math.random() * charCodes.length)];
-
-        passWordCharacters.push(String.fromCharCode(charCode))
-    }
-    return passWordCharacters.join('');
+// The Character Code Generating Function
+function arrayFromLowToHigh(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
 }
-
-
-// Copy Password Button
-
-copyBtnDOM.addEventListener('click', () => {
-    const textArea = document.createElement('textarea');
-    const passwordToCopy = resultDOM.innerText;
-
-    // A case when password is empty
-    if(!passwordToCopy) return;
-
-    // Copy Functionality
-    textArea.value = passwordToCopy;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand("copy");
-    textArea.remove();
-    alert("Password Copied to Clipboard");
-})
