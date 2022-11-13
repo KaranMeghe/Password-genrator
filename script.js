f652d0296adc04400a1108c8a473ecc774314933
Karan Meghe <karanmeghe87@gmail.com>
2022-11-13 18:13:39 +0530

    Getting Dom elements , Chracter Code Genrating Function, Getting character codes For The Application & The Password Genrating function

---
+++ script.js
@@ -0,0 +1,50 @@
+// Getting DOM elements
+const form = document.getElementById('passwordGenratorForm');
+const resultDOM = document.getElementById('result');
+const copyBtnDOM = document.getElementById('copy');
+const lengthDOM = document.getElementById('length');
+const upperCaseDOM = document.getElementById('uppercase');
+const numbersDOM = document.getElementById('numbers');
+const symbolsDOM = document.getElementById('symbols');
+const genratebtn = document.getElementById('genrate');
+
+//Chracter Code Genrating Function
+function arrayFromLowToHigh(low,high){
+    const array = [];
+    for(let i = low; i <= high.length; i++){
+        array.push(i);
+    }
+    return array;
+ } 
+
+// Getting character codes For The Application
+const UPPERCASE_CODES = arrayFromLowToHigh(65,90);
+const LOWERCASE_CODES = arrayFromLowToHigh(97,122);
+const NUMBERS_CODES = arrayFromLowToHigh(48,57);
+const SYMBOLS_CODES = arrayFromLowToHigh(33,47)
+                     .concat(arrayFromLowToHigh(58,64))
+                     .concat(arrayFromLowToHigh(91,96))
+                     .concat(arrayFromLowToHigh(123,126));
+
+
+
+// The Password Genrating function
+let genratePassword = (
+    characterAmount,
+    includeUpperCase,
+    includeNumbers,
+    includeSymbols
+) => {
+    let charCodes = LOWERCASE_CODES;
+    if(includeUpperCase) charCodes = charCodes.concat(UPPERCASE_CODES);
+    if(includeNumbers) charCodes = charCodes.concat(NUMBERS_CODES);
+    if(includeSymbols) charCodes = charCodes.concat(SYMBOLS_CODES);
+
+    const passWordCharacters = [];
+    for(let i = 0; i < characterAmount; i++){
+        const charCode = charCodes[Math.floor(Math.random() * charCodes.length)];
+
+        passWordCharacters.push(String.fromCharCode(charCode))
+    }
+    return passWordCharacters.join('');
+}
